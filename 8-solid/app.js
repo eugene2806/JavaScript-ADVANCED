e23dfc353406d214378637e24dbe64e4e61d129d
Eugene <smilefox2806@gmail.com>
2023-11-02 17:37:58 +0700

    Исправил ошибки

--- 8-solid/app.js
+++ 8-solid/app.js
@@ -14,6 +14,11 @@ class Billing {
 }
 
 class FixBilling extends Billing {
+    constructor(array) {
+        super(array);
+        this.fixBilling();
+    }
+   
      fixBilling() {
         this.amount = this.calculateTotal();
         return this.amount;
@@ -25,6 +30,7 @@ class HourBilling extends Billing {
     constructor(array, hours) {
         super(array);
         this.hours = hours;
+        this.hourBilling();
     }
     hourBilling() {
         this.amount = this.calculateTotal() * this.hours;
@@ -33,18 +39,22 @@ class HourBilling extends Billing {
 }
 
 class ItemBilling extends Billing {
+    constructor(array) {
+        super(array);
+        this.itemBilling();
+    }
     itemBilling() {
         this.amount = this.calculateTotal() * this.array.length;
         return this.amount
     }
 }
 
-const bil = new FixBilling([100, 200, 150]).fixBilling();
-console.log(bil);
-const bil2 = new HourBilling([100, 200, 150], 2).hourBilling();
-console.log(bil2);
-const bil3 = new ItemBilling([100, 200, 150]).itemBilling();
-console.log(bil3);
+const bil = new FixBilling([100, 200, 150]);
+console.log(bil.amount);
+const bil2 = new HourBilling([100, 200, 150], 2);
+console.log(bil2.amount);
+const bil3 = new ItemBilling([100, 200, 150]);
+console.log(bil3.amount);
 
 
 
